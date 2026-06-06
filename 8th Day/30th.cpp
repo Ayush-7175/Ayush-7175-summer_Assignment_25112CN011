aea514b8d874c87c6478eb5af19516b46a7c5f11
unknown <tysontechnopoint@gmail.com>
2026-06-07 05:03:09 +0530

    Day 8

---
+++ 8th Day/30th.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std; 
+int main()
+{   int a=1;
+    int n;
+    cout<<"enter number of rows u want";
+    cin>>n;
+    for(int i=0;i<n;i++)
+    {
+    for(int j=0;j<=i;j++)
+    {
+         cout<<a;
+         a++;
+    }
+    a=1;
+    cout<<endl;
+    }
+    return 0;
+
+}
