d8ddb9cb4c9b64a702ba2a466b9ca7ea6c8a33eb
unknown <tysontechnopoint@gmail.com>
2026-06-06 23:20:42 +0530

    Day 7 started

---
+++ 7th Day/25th.cpp
@@ -0,0 +1,20 @@
+//Write a program to Recursive factorial
+#include<bits/stdc++.h>
+using namespace std;
+int factorial(int n)
+{    while(n!=1)
+    {
+    int fact=n*factorial(n-1);
+    return fact;
+}
+}
+int main(){
+
+    int n;
+    cin>>n;
+    int ans=factorial(n);
+    cout<<ans;
+    return 0;
+
+    
+}
