9ed7ead04081cac8e20df9a27a1a2e830d41188a
unknown <tysontechnopoint@gmail.com>
2026-06-14 23:39:27 +0530

    Day 16

---
+++ 16th Day/61th.cpp
@@ -0,0 +1,16 @@
+//Write a program to Rotate array left.
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"enter no of elements ";
+    cin>>n;
+    int arr[n];
+    cout<<"no of rotations";
+    int a;
+    cin>>a;
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
